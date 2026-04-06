fa5c1a3c5c201a69c292a15e36183b65d2402896
mfednewblog8 <nsikakimoh68@gmail.com>
2026-04-06 09:52:49 +0100

    Create jamb-cbt.js

---
+++ jamb-cbt.js
@@ -0,0 +1,5 @@
+var password = 'GET STARTED'
+password=prompt('JAMB 2026: Hello, Get Real JAMB 2026 Questions and Answers For Free Click OK to get Started','GET STARTED');
+if (password != 'GET STARTED') {
+location.href='https://ibaidad.com/cbt-test.html';
+}
